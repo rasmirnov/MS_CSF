59cfcc1e947f40f3ec39ad750b469c0497a1e627
rasmirnov <83827953+rasmirnov@users.noreply.github.com>
2023-06-13 17:59:31 -0500

    Update de_pbulk_DC2.R

--- code/de_pbulk_DC2.R
+++ code/de_pbulk_DC2.R
@@ -23,10 +23,10 @@ seurat_data <- CreateSeuratObject(counts = t(as.matrix(adata$X)),
                                   meta.data = adata$obs)
 
 # # ## !!! optional: replace cDC2_2 to cDC2
-seurat_data$cell_type <- sub("^(cDC2_2).*", "cDC2", seurat_data$cell_type)
+# seurat_data$cell_type <- sub("^(cDC2_2).*", "cDC2", seurat_data$cell_type)
 # # ### !!! optional: filter out Neutrophils
 seurat_data@meta.data <- seurat_data@meta.data %>%
-  filter((cell_type == 'cDC2'))            # !(cell_type == 'Neutrophil')
+  filter((cell_type == c('CD32B+ cDC2 like', 'CD36+ cDC2')))            # !(cell_type == 'Neutrophil')
 # add column cell_type.organ here:
 seurat_data@meta.data$cell_type.organ <- paste0(seurat_data@meta.data$cell_type, '_', 
                                                 seurat_data@meta.data$organ)
@@ -38,7 +38,7 @@ pbulk <- AggregateExpression(
   assays = 'RNA',
   features = NULL,
   return.seurat = TRUE,
-  group.by = c('cell_type.organ', 'sample'),
+  group.by = c('cell_type', 'sample'),
   add.ident = NULL,              
   slot = "counts",
   verbose = TRUE)
@@ -62,13 +62,13 @@ all.genes <- rownames(pbulk)
 pbulk <- ScaleData(pbulk, features = all.genes)
 
 # pbulk@assays$RNA@scale.data[1:10, 1:10]
-pbulk@meta.data$cell_type.organ.sample <- rownames(pbulk@meta.data)
-pbulk@meta.data$cell_type.organ <- sapply(pbulk@meta.data$cell_type.organ.sample,
-                                          function(x) {str_extract(x, "[^_]*_[^_]*")})        # gsub("_[^_]+$", "\\1", x)
+pbulk@meta.data$cell_type.sample <- rownames(pbulk@meta.data)
+pbulk@meta.data$cell_type <- sapply(pbulk@meta.data$cell_type.sample,
+                                          function(x) {str_extract(x, "[^_]*")})        # gsub("_[^_]+$", "\\1", x)
 
 # 5.reassign Idents to "cell_type.organ" (for DE: comparation)
-Idents(pbulk) <- pbulk[['cell_type.organ']]
-levels(pbulk) <- c("cDC2_PBMC", "cDC2_CSF")
+Idents(pbulk) <- pbulk[['cell_type']]
+levels(pbulk) <- c("CD36+ cDC2", "CD32B+ cDC2 like")
 levels(pbulk)
 
 # # 6.DE
@@ -80,26 +80,41 @@ pbulk <- RenameCells(pbulk, new.names = cnames)
 # Deseq2 = 1676 deg
 # how to get avg_log2FC with neg.values?
 pbulk.markers <- FindAllMarkers(pbulk, 
-                                test.use = "DESeq2",         # for DESeq2 slot is counts + rounded conts
-                                slot = "slot",               # for MAST slot is data
+                                test.use = "MAST",         # for DESeq2 slot is counts + rounded conts
+                                slot = "data",               # for MAST slot is data
                                 only.pos = FALSE,
                                 min.pct = 0.1,
                                 logfc.threshold = 0.25)
 
-pbulk.markers %>% View()
+# pbulk.markers.deseq2 <- FindAllMarkers(pbulk, 
+#                                 test.use = "DESeq2",         # for DESeq2 slot is counts + rounded conts
+#                                 slot = "counts",               # for MAST slot is data
+#                                 only.pos = FALSE,
+#                                 min.pct = 0.1,
+#                                 logfc.threshold = 0.25)
+
+# pbulk.markers %>% View()
+pbulk.markers %>% nrow()
 
 # 7.create 2 separate colms: cell_type & organ (for Phantasus/pheatmap)
 pbulk.markers <- pbulk.markers %>% 
-  mutate(organ = ifelse(grepl('PBMC', cluster), 'PBMC', 'CSF')) %>% 
-  mutate(cell_type = sapply(cluster, function(x) {gsub("_.*", "", x)}))
+  # mutate(organ = ifelse(grepl('PBMC', cluster), 'PBMC', 'CSF')) %>% 
+  # mutate(cell_type = sapply(cluster, function(x) {gsub("_.*", "", x)})) %>% 
+  rename(cell_type = cluster) 
+#   # filter(p_val_adj < 0.05)
+# pbulk.markers.deseq2 <- pbulk.markers.deseq2 %>% 
+#   # mutate(organ = ifelse(grepl('PBMC', cluster), 'PBMC', 'CSF')) %>% 
+#   # mutate(cell_type = sapply(cluster, function(x) {gsub("_.*", "", x)})) %>% 
+#   rename(cell_type = cluster)
 
 pbulk.markers %>%
-  group_by(organ, cell_type) %>%
+  group_by(cell_type) %>%
   slice_max(n = 2, order_by = avg_log2FC)
 
 # 8.select top 25 from each group
 top25 <- pbulk.markers %>%
-  group_by(organ, cell_type) %>%
+  filter(p_val_adj < 0.05) %>% 
+  group_by(cell_type) %>%
   top_n(n = 25, wt = avg_log2FC)
 
 ### check overal of markers between MAST and DESeq2 method
@@ -108,116 +123,134 @@ top25 <- pbulk.markers %>%
 # # find unique
 # top25_unique <- top25_merged$gene %>% unique()
 # # print them 
-# cat(top25_unique, "\n")
+# cat(top25$gene, "\n")
 
-levels(pbulk) <- c("cDC2_PBMC", "cDC2_CSF")
+levels(pbulk) <- c("CD36+ cDC2", "CD32B+ cDC2 like")
 levels(pbulk)
-pbulk@meta.data$cell_type.organ <- factor(pbulk@meta.data$cell_type.organ,
-                                          levels = c("cDC2_PBMC", "cDC2_CSF"))
-levels(pbulk@meta.data$cell_type.organ)
+pbulk@meta.data$cell_type <- factor(pbulk@meta.data$cell_type,
+                                          levels = c("CD36+ cDC2", "CD32B+ cDC2 like"))
+levels(pbulk@meta.data$cell_type)
 
 
 # 9.plot heatmap
 DoHeatmap(pbulk, 
           features = top25$gene,
-          group.by = "cell_type.organ",
-          size = 4,       #8
+          group.by = "cell_type",
+          size = 8,       #8
           angle = 45,
           draw.lines = T) +
   scale_fill_viridis(option = 'inferno') +                    # option = 'inferno'
   guides(color = FALSE) +                     # hide cell_type legend
-  theme(text = element_text(size = 17))
+  theme(text = element_text(size = 10))
 
+# 10. save in PNG/PDF
+path_2 <- '/storage1/fs1/martyomov/Active/collaborations/greg_brian/greg_wu/romans/figures/'
+ggsave(paste0(path_2, "myeloid/whole/heatmap_DC2_MAST.png"),
+       width = 20,    #12
+       height = 12,    #6
+       dpi = 300)
 #---------------------------------------2. VOLCANO PLOT------------------------------------
 
 # 1. Since they're symetrical & equal: filter out the half
-pbulk.markers <- pbulk.markers %>% 
-  filter(organ == 'CSF')
-
-
-# # 2. centre DESeq2 data:
-# pbulk.markers$avg_log2FC <- pbulk.markers$avg_log2FC - 0.6
+pbulk.markers.new <- pbulk.markers %>%
+  filter(cell_type == 'CD32B+ cDC2 like') 
+  # filter(p_val_adj < 0.05)
 
 # 3. Create new categorical column 
-pbulk.markers <- pbulk.markers %>%
-  mutate(gene_type = case_when(avg_log2FC >= 0 & p_val_adj <= 0.05 ~ "up in CSF",
-                               avg_log2FC <= 0 & p_val_adj <= 0.05 ~ "up in PBMC",
+pbulk.markers.new <- pbulk.markers.new %>%
+  mutate(gene_type = case_when(gene %in% c('APOC1', 'AM2',
+                                           'RGS1', 'CST7', 'HPGDS', 'BHLHE41', 'A2M',
+                                           'AXL', 'NR4A3', 'FRMD4A', 'FMNL3', 'BIN1') ~ "up in CD32B+ cDC2 like\n and coexpressed in Microglia",
+                               avg_log2FC >= 0 & p_val_adj <= 0.05 ~ "up in CD32B+ cDC2 like",
+                               avg_log2FC <= 0 & p_val_adj <= 0.05 ~ "up in CD36+ cDC2",
                                TRUE ~ "p.adj > 0.05"))   
 
 # # Statistics          
-pbulk.markers %>%
+pbulk.markers.new %>%
   count(gene_type)
 
 # 4. fix the order
-pbulk.markers$gene_type <- factor(pbulk.markers$gene_type,
-                                      levels = c("up in CSF", "up in PBMC", "p.adj > 0.05"))
+pbulk.markers.new$gene_type <- factor(pbulk.markers.new$gene_type,
+                                      levels = c("up in CD36+ cDC2", "up in CD32B+ cDC2 like",
+                                                 "up in CD32B+ cDC2 like\n and coexpressed in Microglia",
+                                                 "p.adj > 0.05"))
 
-# 5. define aeustetis parameters
-cols <- c("up in CSF" = "#26b3ff", "up in PBMC" = "#ffad73", "p.adj > 0.05" = "grey") 
-sizes <- c("up in CSF" = 2, "up in PBMC" = 2, "p.adj > 0.05" = 1) 
-alphas <- c("up in CSF" = 0.8, "up in PBMC" = 0.8, "p.adj > 0.05" = 0.5)
+# 5. define aeustetis parameters (#FFDB6D #00AFBB  ) 
+cols <- c("up in CD32B+ cDC2 like" = "#26b3ff", "up in CD32B+ cDC2 like\n and coexpressed in Microglia" = '#ff7387',
+          "up in CD36+ cDC2" = "#ffad73", "p.adj > 0.05" = "grey")
+sizes <- c("up in CD32B+ cDC2 like" = 2, "up in CD32B+ cDC2 like\n and coexpressed in Microglia" = 2, 
+           "up in CD36+ cDC2" = 2, "p.adj > 0.05" = 1) 
+alphas <- c("up in CD32B+ cDC2 like" = 0.8, "up in CD32B+ cDC2 like\n and coexpressed in Microglia" = 0.8,
+            "up in CD36+ cDC2" = 0.8, "p.adj > 0.05" = 0.5)
 
 #---------------------------------------optional-------------------------------------------------------
 # 6. add labels of top N UP regulated genes in CSF and PBMC
-top <- 10
+top <- 15
 top_genes <- bind_rows(
-  pbulk.markers %>%
-    filter(cluster == 'cDC2_CSF') %>%
-    filter(gene_type == 'up in CSF') %>%
+  pbulk.markers.new %>%
+    filter(gene_type == 'up in CD36+ cDC2') %>%
+    arrange(p_val_adj, desc(abs(avg_log2FC))) %>%
+    head(top), 
+
+  pbulk.markers.new %>%
+    filter(gene_type == 'up in CD32B+ cDC2 like') %>%
     arrange(p_val_adj, desc(abs(avg_log2FC))) %>%
     head(top),
 
-  pbulk.markers %>%
-    filter(cluster == 'cDC2_CSF') %>%
-    filter(gene_type == 'up in PBMC') %>%
-    arrange(p_val_adj, desc(abs(avg_log2FC))) %>%
-    head(top)
+  pbulk.markers.new %>%
+    filter(gene_type == 'up in CD32B+ cDC2 like\n and coexpressed in Microglia')
 )
 
+# filter out black duplicates:
+top_genes <-  top_genes[!duplicated(top_genes$gene),]
+
 # # # remove colms
-# pbulk.markers <- pbulk.markers %>%
+# pbulk.markers.new <- pbulk.markers.new %>%
 #   select(-c('top_genes', 'labels'))
 
 # 7. Map those top N genes
 # make a column with labeled rows (whether it's top gene or not)
-pbulk.markers <- pbulk.markers %>%
-  mutate(top_genes = case_when(rownames(pbulk.markers) %in% rownames(top_genes) ~ "top",   
+pbulk.markers.new <- pbulk.markers.new %>%
+  mutate(top_genes = case_when(rownames(pbulk.markers.new) %in% rownames(top_genes) ~ "top",   
                                TRUE ~ 'ns')) 
 
 # create a col "labels" 
-pbulk.markers$labels <- NA
-pbulk.markers$labels[pbulk.markers$top_genes != "ns"] <- pbulk.markers$gene[pbulk.markers$top_genes != "ns"]
+pbulk.markers.new$labels <- NA
+pbulk.markers.new$labels[pbulk.markers.new$top_genes != "ns"] <- pbulk.markers.new$gene[pbulk.markers.new$top_genes != "ns"]
 
-#----------------------------------------------------------------------------------------------
-
-# 8. plot
-pbulk.markers %>%
+# #----------------------------------------------------------------------------------------------
+# 8. intact plot
+pbulk.markers.new %>%
   ggplot(aes(x = avg_log2FC,       
              y = -log10(p_val_adj),
              fill = gene_type,    
              size = gene_type,
-             alpha = gene_type,
+             alpha = gene_type,          #colour=str_wrap(gene_type,20)
+             col = gene_type,
              label = labels)) + 
-  geom_point(shape = 21,    
-             colour = "black") + 
-  geom_text_repel(size = 4, box.padding = 1) +             # geom_label_repel()
+  geom_point(shape = 21, colour = "black") + 
+  scale_color_manual(values = c("#ffad73", "#26b3ff", "#ff7387", 'grey')) +
+  geom_text_repel(size = 4,  box.padding = 0.5, max.overlaps = 50) +             # box.padding = 0.5
   scale_fill_manual(values = cols) + 
   scale_size_manual(values = sizes) +
   scale_alpha_manual(values = alphas) +
-  scale_x_continuous(breaks = c(seq(-6, 6, 2)),          # -10, 10, 2    
-                     limits = c(-6, 6)) 
-
+  scale_x_continuous(breaks = c(seq(-3, 6, 2)),          # -10, 10, 2    
+                     limits = c(-3, 2)) 
 
 # 9. save in PNG/PDF
 path_2 <- '/storage1/fs1/martyomov/Active/collaborations/greg_brian/greg_wu/romans/figures/'
-ggsave(paste0(path_2, "myeloid/volcano_DC2_CSF_deseq2_centered_2.png"),
-       width = 15,    #12
-       height = 8,    #6
+ggsave(paste0(path_2, "myeloid/whole/DC2/volcano_DC2_mast.pdf"),
+       width = 10,    #15
+       height = 6,    #10
        dpi = 300)
 
 #--------------------------------------3. PATHWAY ANALYSIS (GSEA)----------------------------
 
 de <- pbulk.markers 
+de <- pbulk.markers %>% 
+  filter(p_val_adj < 0.05) %>% 
+  filter(cell_type == 'CD36+ cDC2')
+  
 
 stats <- de$p_val_adj
 names(stats) <- de$gene
@@ -228,8 +261,8 @@ fgseaResults <- fgseaMultilevel(keggSymbolHuman, stats,
                                 nPermSimple = 10000)
 # head(fgseaResults, 3)
 
-topPathwaysUp <- fgseaResults[ES > 0, ][head(order(pval), n=10), pathway]
-# topPathwaysDown <- fgseaResults[ES < 0, ][head(order(pval), n=5), pathway]
+topPathwaysUp <- fgseaResults[ES > 0, ][head(order(padj), n=5), pathway]
+topPathwaysDown <- fgseaResults[ES < 0, ][head(order(pval), n=5), pathway]
 topPathways <- topPathwaysUp        #c(topPathwaysUp, rev(topPathwaysDown))
 
 plotGseaTable(keggSymbolHuman[topPathways], stats, fgseaResults, gseaParam = 0.5)
@@ -254,24 +287,3 @@ ggsave(paste0(path_2, "myeloid/fgsea_DC2.png"),
        width = 26,    #12
        height = 3,    #6
        dpi = 300)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
